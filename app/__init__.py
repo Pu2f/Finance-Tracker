6fe73428b68ea6e88e75c0380e19e16232bab585
Pu2f <chatchanok.nt@gmail.com>
2026-02-20 22:31:13 +0700

    add __init__

---
+++ app/__init__.py
@@ -0,0 +1,41 @@
+from flask import Flask
+
+from config import Config
+
+from .extensions import csrf, db, login_manager, migrate
+from .models import User
+
+
+def create_app(config_object=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_object)
+
+    # init extensions
+    csrf.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    login_manager.init_app(app)
+    login_manager.login_view = "auth.login"
+
+    @login_manager.user_loader
+    def load_user(user_id: str):
+        return User.query.get(int(user_id))
+
+    # register blueprints
+    from .auth.routes import auth_bp
+    from .transactions.routes import tx_bp
+    from .categories.routes import cat_bp
+    from .dashboard.routes import dash_bp
+
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(tx_bp)
+    app.register_blueprint(cat_bp)
+    app.register_blueprint(dash_bp)
+
+    # simple home route
+    @app.get("/")
+    def home():
+        return "<a href='/dashboard'>Go to Dashboard</a>"
+
+    return app
